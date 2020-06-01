9ea3d725ae0cb09ef9087f61398cb366c2d004c4
Duc Nguyen <ducxomme@gmail.com>
2020-06-01 22:53:11 +0900

    12-2 name length

--- 12-server-side-validation/controllers/user.controller.js
+++ 12-server-side-validation/controllers/user.controller.js
@@ -16,10 +16,14 @@ module.exports.postCreate = (req, res) => {
    if (!req.body.name)  {
       errors.push('Name is required. ');
    }
+   if (req.body.name.length > 30) {
+      errors.push('Name too length. Must below 30 characters');
+   }
 
    if (!req.body.phone) {
       errors.push('Phone is required. ');
    }
+
    if (errors.length > 0) {
       res.render('users/create', {
          errors: errors,

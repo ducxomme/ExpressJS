7d352def21a4786a0a75a9386bd1cc8b03bd9806
Duc Nguyen <ducxomme@gmail.com>
2020-06-02 08:12:12 +0900

    13-middleware

---
+++ 13-middleware/controllers/user.controller.js
@@ -0,0 +1,47 @@
+const db = require("../db");
+const shortid = require("shortid");
+
+module.exports.index = (req, res) => {
+  res.render("users/index", {
+    users: db.get("users").value(),
+  });
+};
+
+module.exports.create = (req, res) => {
+	res.render("users/create");
+};
+
+module.exports.postCreate = (req, res) => {
+  req.body.id = shortid.generate();
+  db.get("users").push(req.body).write();
+  res.redirect("/users");
+};
+
+module.exports.get = (req, res) => {
+	let user = db.get("users").find({ id: req.params.id }).value();
+	if (user) {
+		res.render("users/view", {
+		  user: user,
+		});
+	}
+};
+
+module.exports.update = (req, res) => {
+  res.render("users/update", {
+    user: db.get("users").find({ id: req.params.id }).value(),
+  });
+};
+
+module.exports.postUpdate = (req, res) => {
+	console.log(res.locals);
+	db.get("users")
+    .find({ id: req.body.id })
+    .assign({ name: req.body.name })
+    .write();
+  res.redirect("/users");
+};
+
+module.exports.delete = (req, res) => {
+  db.get("users").remove({ id: req.params.id }).write();
+  res.redirect("/users");
+};

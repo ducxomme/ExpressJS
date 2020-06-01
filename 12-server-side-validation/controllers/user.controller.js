857031e3c3632c98aa4c9a725bd046d0c70a9f5c
Duc Nguyen <ducxomme@gmail.com>
2020-06-01 22:49:48 +0900

    12-validation

---
+++ 12-server-side-validation/controllers/user.controller.js
@@ -0,0 +1,58 @@
+const db = require("../db");
+const shortid = require("shortid");
+
+module.exports.index = (req, res) => {
+  res.render("users/index", {
+    users: db.get("users").value(),
+  });
+};
+
+module.exports.create = (req, res) => {
+  res.render("users/create");
+};
+
+module.exports.postCreate = (req, res) => {
+   var errors = [];
+   if (!req.body.name)  {
+      errors.push('Name is required. ');
+   }
+
+   if (!req.body.phone) {
+      errors.push('Phone is required. ');
+   }
+   if (errors.length > 0) {
+      res.render('users/create', {
+         errors: errors,
+         values: req.body
+      })
+   }
+   req.body.id = shortid.generate();
+   db.get("users").push(req.body).write();
+   res.redirect("/users");
+};
+
+module.exports.get = (req, res) => {
+  res.render("users/view", {
+    user: db.get("users").find({ id: req.params.id }).value(),
+  });
+};
+
+module.exports.update = (req, res) => {
+  res.render("users/update", {
+    user: db.get("users").find({ id: req.params.id }).value(),
+  });
+};
+
+module.exports.postUpdate = (req, res) => {
+  
+  db.get("users")
+    .find({ id: req.body.id })
+    .assign({ name: req.body.name })
+    .write();
+  res.redirect("/users");
+};
+
+module.exports.delete = (req, res) => {
+  db.get("users").remove({ id: req.params.id }).write();
+  res.redirect("/users");
+};
---
+++ 12-server-side-validation/routes/user.route.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const controller = require('../controllers/user.controller');
+
+router.get("/", controller.index);
+
+
+router.get("/create", controller.create);
+
+router.get("/:id", controller.get);
+
+router.post("/create", controller.postCreate);
+
+
+router.get("/:id/update", controller.update);
+
+router.post("/update", controller.postUpdate);
+
+router.get('/:id/delete', controller.delete)
+
+module.exports = router;
